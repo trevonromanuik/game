5fbf12591ff4a3395b92b02137510eb5a0b24d5e
Trevon Romanuik <trevon.romanuik@gmail.com>
2017-11-07 16:32:24 -0700

    beefed up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
 let gulp = require('gulp');
+let del = require('del');
 let webpack = require('webpack-stream');
-gulp.task('build', () => {
+
+gulp.task('clean', () => {
+    return del(['dist']);
+});
+
+gulp.task('copy', ['clean'], () => {
+    return gulp.src('src/game.html')
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('webpack', ['clean'], () => {
     return gulp.src('src/index.js')
         .pipe(webpack(require('./webpack.config.js')))
         .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('build', ['copy', 'webpack']);
+
+gulp.task('default', ['build']);
